ee264738fc56b2b170898e9bd46ec9f40e9c7123
gianvalen <157687026+gianvalen@users.noreply.github.com>
2024-03-14 23:05:53 +0800

    Updated urls and templates

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('list', RecipeListView.as_view(), name='recipe_list'),
-    path('<int:pk>', RecipeDetailView.as_view(), name='recipe_detail')
+    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail')
 ]
 
 app_name = 'ledger'

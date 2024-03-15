755ef643754c54627d5fe4b2eac516335cac9a6f
gianvalen <157687026+gianvalen@users.noreply.github.com>
2024-03-15 12:59:46 +0800

    Added author, created_on, and updated_on fields to Recipe

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Ingredient(models.Model):
@@ -11,6 +12,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from .models import Recipe
-
-def index(request):
-    return HttpResponse('Hello World!')
-
-def recipe_list(request):
-    recipes = Recipe.objects.all()
-    ctx = {"recipes": recipes}
-
-    return render(request, 'recipe_list.html', ctx)
-
-def recipe_detail(request, pk):
-    recipe = Recipe.objects.get(pk=pk)
-    ctx = {'recipe': recipe}
-
-    return render(request, 'recipe_detail.html', ctx)
 
 
 class RecipeListView(ListView):
